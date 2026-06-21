d1c3478d76c1207e579eb442a194d153d16cda4c
Talaxy009 <x009ba1@gmail.com>
2026-06-21 16:15:36 +0800

    🐛 尝试修复 React Error 418

--- src/components/BuildTime.tsx
+++ src/components/BuildTime.tsx
@@ -1,3 +1,5 @@
+import { useEffect, useState } from 'react';
+
 import { getTimeDiff, toISOString } from '../utils/dataUtils';
 
 type Props = {
@@ -5,7 +7,11 @@ type Props = {
 };
 
 export default function BuildTimeFromNow({ buildTime }: Props) {
-	return (
-		<time dateTime={toISOString(buildTime)}>{getTimeDiff(buildTime)}</time>
-	);
+	const [timeStr, setTimeStr] = useState('几秒前');
+
+	useEffect(() => {
+		setTimeStr(getTimeDiff(buildTime));
+	}, [buildTime]);
+
+	return <time dateTime={toISOString(buildTime)}>{timeStr}</time>;
 }
